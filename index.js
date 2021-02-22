085bc1e450eaeda750be271d1e7ac16512939098
Soo-Beom <soobeomkim77@gmail.com>
2021-02-22 11:57:40 -0800

    commit tree 42c2cca3a958ed91c4f50566c7cd9e53114ceaa2

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  if (distance <= 400) {
+    return "This one is on me!"
+  }
+    else if  (distance > 2000 && distance < 2500) {
+      return "I will gladly take your thirty bucks."
+    }
+      else (distance > 2500) ;{
+        return "No can do."
+      }
+}   
+
+function ternaryCheckCity(cityName){
+  if (cityName === "NYC") {
+    return "Ok, sounds good.";
+  }
+    else {
+      return "No go."
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+  if (tip === "generous") {
+    return "Thank you so much."
+  }
+    else if (tip === "not as generous") {
+      return "Thank you."
+    }
+      else {
+        return "Bye."
+      }
 }
